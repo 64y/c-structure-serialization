28fe68a4af2f4c9b83edeb86675e29ea1615a427
64y <p.yarashevich@gmail.com>
2021-10-07 23:12:36 +0300

    editing binary

--- res/src/_structure/structure_methods.h
+++ res/src/_structure/structure_methods.h
@@ -8,6 +8,16 @@ extern void (*to_string_process_methods[]) (FILE *structure_string_stream, Point
 extern void (*json_encode_process_methods[]) (FILE *structure_json_stream, PointerDictionary *pointerDictionary, void *structure);
 extern void (*json_decode_process_methods[]) (FILE *structure_json_stream, PointerDictionary *pointerDictionary, void *structure);
 
+typedef enum Methods {
+	TO_STRING,
+	JSON_ENCODE,
+	JSON_DECODE,
+	BINARY_ENCODE,
+	BINARY_DECODE
+} Methods;
+
+extern void (*methods[][5]) (FILE *, PointerDictionary *, void *);
+
 char * to_string(Pointer *pointer);
 char * json_encode(Pointer *pointer);
 void * json_decode(char *structure_json, Pointer *pointer);
